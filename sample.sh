99c59e3ced593cac81c64540cea56faa0ec4dbae
Hu Yu <55074512+yuhuUSTC@users.noreply.github.com>
2025-02-26 18:09:13 +0800

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,30 +1,30 @@
 ### ---------------------------- FAR_Base ----------------------------  ###
-# HF_ENDPOINT=https://hf-mirror.com torchrun --nnodes=1 --nproc_per_node=8  main_far.py \
-# --img_size 256 --vae_path /video_hy2/modelzoo/yuhu_ckpt/pretrained/vae_mar/kl16.ckpt --vae_embed_dim 16 --vae_stride 16 --patch_size 1 \
+# torchrun --nnodes=1 --nproc_per_node=1  main_far.py \
+# --img_size 256 --vae_path pretrained/vae_mar/kl16.ckpt --vae_embed_dim 16 --vae_stride 16 --patch_size 1 \
 # --model far_base --diffloss_d 6 --diffloss_w 1024 \
-# --eval_bsz 32 --num_images 50000 \
+# --eval_bsz 32 --num_images 1000 \
 # --num_iter 10 --num_sampling_steps 100 --cfg 3.0 --cfg_schedule linear --temperature 1.0 \
-# --output_dir /ossfs/workspace/yuhu.yh/code/FAR/logs \
-# --resume /video_hy2/modelzoo/yuhu_ckpt/pretrained/FAR \
-# --data_path /mnt/workspace/workgroup/yuhu/data/imagenet-1k --evaluate --mask
+# --output_dir pretrained_models/far/far_base \
+# --resume pretrained_models/far/far_base \
+# --data_path ${IMAGENET_PATH} --evaluate
 
 ### ---------------------------- FAR_Large ----------------------------  ###
-HF_ENDPOINT=https://hf-mirror.com torchrun --nnodes=1 --nproc_per_node=1  main_far.py \
---img_size 256 --vae_path /video_hy2/modelzoo/yuhu_ckpt/pretrained/vae_mar/kl16.ckpt --vae_embed_dim 16 --vae_stride 16 --patch_size 1 \
+torchrun --nnodes=1 --nproc_per_node=1  main_far.py \
+--img_size 256 --vae_path pretrained/vae_mar/kl16.ckpt --vae_embed_dim 16 --vae_stride 16 --patch_size 1 \
 --model far_large --diffloss_d 3 --diffloss_w 1024 \
 --eval_bsz 32 --num_images 1000 \
 --num_iter 10 --num_sampling_steps 100 --cfg 3.0 --cfg_schedule linear --temperature 1.0 \
---output_dir /ossfs/workspace/yuhu.yh/code/FAR/logs \
---resume /video_hy2/modelzoo/yuhu_ckpt/pretrained/FAR \
---data_path /mnt/workspace/workgroup/yuhu/data/imagenet-1k --evaluate
+--output_dir pretrained_models/far/far_large \
+--resume pretrained_models/far/far_large \
+--data_path ${IMAGENET_PATH} --evaluate
 
 ### ---------------------------- FAR_Huge ----------------------------  ###
-# HF_ENDPOINT=https://hf-mirror.com torchrun --nnodes=1 --nproc_per_node=8  main_far.py \
-# --img_size 256 --vae_path /video_hy2/modelzoo/yuhu_ckpt/pretrained/vae_mar/kl16.ckpt --vae_embed_dim 16 --vae_stride 16 --patch_size 1 \
+# torchrun --nnodes=1 --nproc_per_node=1  main_far.py \
+# --img_size 256 --vae_path pretrained/vae_mar/kl16.ckpt --vae_embed_dim 16 --vae_stride 16 --patch_size 1 \
 # --model far_huge --diffloss_d 3 --diffloss_w 1024 \
-# --eval_bsz 32 --num_images 50000 \
+# --eval_bsz 32 --num_images 1000 \
 # --num_iter 10 --num_sampling_steps 100 --cfg 3.0 --cfg_schedule linear --temperature 1.0 \
-# --output_dir /ossfs/workspace/yuhu.yh/code/FAR/logs \
-# --resume /video_hy2/modelzoo/yuhu_ckpt/pretrained/FAR \
-# --data_path /mnt/workspace/workgroup/yuhu/data/imagenet-1k --evaluate --mask
+# --output_dir pretrained_models/far/far_huge \
+# --resume pretrained_models/far/far_huge \
+# --data_path ${IMAGENET_PATH} --evaluate
 
